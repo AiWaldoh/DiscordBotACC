45a903406ff3e1d5c6ebede31503d84b3bb6a72c
Waldoh <waldoh@pop-os.lan>
2024-03-08 19:52:47 -0400

    added logging

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 import pytz
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
 
@@ -20,6 +21,9 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 timezone = pytz.timezone('Canada/Atlantic')
 
 events = []
+
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(module)s - %(funcName)s - %(lineno)d - %(message)s')
 
 def create_event_message(event, footer_message):
     if not isinstance(event, discord.ScheduledEvent):
@@ -41,16 +45,23 @@ def create_event_message(event, footer_message):
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-    load_events.start()
-    check_event_times.start()
+    logging.info(f'Logged in as {bot.user.name}')
+    try:
+        load_events.start()
+        check_event_times.start()
+    except Exception as e:
+        logging.error(f'Error starting tasks: {str(e)}')
 
 @tasks.loop(hours=1)
 async def load_events():
     global events
     guild = bot.get_guild(NOTIFICATION_GUILD_ID)
     if guild:
-        events = await guild.fetch_scheduled_events()
+        try:
+            events = await guild.fetch_scheduled_events()
+            logging.info(f'Loaded {len(events)} events from guild: {guild.name}')
+        except Exception as e:
+            logging.error(f'Error loading events: {str(e)}')
 
 @tasks.loop(minutes=1)
 async def check_event_times():
@@ -62,15 +73,14 @@ async def check_event_times():
         for event in events:
             event_start_time = event.start_time.astimezone(timezone)
             time_diff = event_start_time - current_time
-            print(f"Event: {event.name}, Time Diff: {time_diff.total_seconds() / 60} minutes")
 
             for notification_hour in NOTIFICATION_HOURS:
                 notification_time = timedelta(hours=notification_hour)
                 if notification_time < time_diff <= notification_time + timedelta(minutes=1):
-                    print(f"Sending message {notification_hour}h before the event")
                     footer_message = f"Starts in {notification_hour} hour{'s' if notification_hour > 1 else ''}"
                     embed = create_event_message(event, footer_message)
                     await channel.send(embed=embed)
+                    logging.info(f'Sent notification for event: {event.name}')
 
 # Run the bot
 bot.run(DISCORD_BOT_TOKEN)
